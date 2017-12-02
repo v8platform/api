888fe8adee7279dcf7d9f0628911891c9dff2d45
Khorevaa <khorevaa@gmail.com>
2017-12-03 00:16:30 +0300

    Сделал класс чтения настроек конфига 1С

--- v8config-file/v8config-file.go
+++ v8config-file/v8config-file.go
@@ -1,13 +1,66 @@
 package КонфигурацияСтартера
 
-func Открыть() {
+import (
+	"bufio"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
 
+type настройкиСтартера struct {
+	содержание map[string][]string
 }
 
-func ПолучитьСписок(ключ string) (s []string) {
+func ПрочитатьНастройкиСтартера(pathToFile string) (r *настройкиСтартера, err error) {
 
-	//s = append(s, "что-то нашли")
+	r = &настройкиСтартера{}
+	err = r.открыть(pathToFile)
+
+	return
+}
+
+func (s *настройкиСтартера) открыть(pathToFile string) (err error) {
+
+	b, err := ioutil.ReadFile(pathToFile) // just pass the file name
+	if err != nil {
+		fmt.Print(err)
+		return
+	}
+
+	s.содержание, err = строкаВСоответсвие(string(b), "=")
 
 	return
 
+}
+
+func (s *настройкиСтартера) ПолучитьНастройку(ключНастройки string) (r []string) {
+
+	r = s.содержание[strings.ToUpper(ключНастройки)]
+
+	return
+
+}
+
+func строкаВСоответсвие(s string, sep string) (Соответсвие map[string][]string, err error) {
+
+	if len(sep) == 0 {
+		sep = "="
+	}
+
+	Соответсвие = make(map[string][]string)
+
+	scanner := bufio.NewScanner(strings.NewReader(s))
+	for scanner.Scan() {
+
+		z := strings.Split(scanner.Text(), sep)
+
+		Соответсвие[strings.ToUpper(z[0])] = append(Соответсвие[strings.ToUpper(z[0])], z[1])
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "reading standard input:", err)
+	}
+
+	return
 }
--- v8config-file/v8config-file_test.go
+++ v8config-file/v8config-file_test.go
@@ -1,1 +1,39 @@
 package КонфигурацияСтартера
+
+import (
+	"os"
+	"path"
+	"testing"
+
+	. "gopkg.in/check.v1"
+)
+
+var pwd, _ = os.Getwd()
+
+var ПутьКФайлуКофигурации = path.Join(pwd, "..", "tests", "fixtures", "config.cfg")
+
+// Hook up gocheck into the "go test" runner.
+func Test(t *testing.T) { TestingT(t) }
+
+type ПроверкаБазовойФункциональности struct{}
+
+var _ = Suite(&ПроверкаБазовойФункциональности{})
+
+func (s *ПроверкаБазовойФункциональности) TestЧтениеФайла(c *C) {
+
+	настройка, err := ПрочитатьНастройкиСтартера(ПутьКФайлуКофигурации)
+
+	c.Assert(err, IsNil, Commentf("Ошибка чтения файла: %v", err))
+
+	if err != nil {
+		return
+	}
+
+	СписокПутей := настройка.ПолучитьНастройку("InstalledLocation")
+
+	c.Check(cap(СписокПутей), Equals, 2)
+
+	c.Assert(СписокПутей[0], Equals, "C:\\Program Files\\1cv82")
+	c.Assert(СписокПутей[1], Equals, "C:\\Program Files\\1cv83")
+
+}
