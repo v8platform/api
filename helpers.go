45a2fd7cf5e4039623f30b53d70297a7eb7154ce
Aleksey Khorev <horevaa@yandex.ru>
2020-11-20 23:27:33 +0300

    new: parse connecting string for Infobase

--- helpers.go
+++ helpers.go
@@ -1,7 +1,9 @@
 package v8
 
 import (
+	"github.com/v8platform/errors"
 	"io/ioutil"
+	"strings"
 )
 
 ////////////////////////////////////////////////////////
@@ -23,4 +25,138 @@ func NewTempFile(dir, pattern string) string {
 
 	return tempFile.Name()
 
+}
+
+var ErrorParseConnectingString = errors.BadConnectString.New("wrong connecting string")
+
+func InfobaseFromConnectingStringOrNil(connectingString string) Infobase {
+	ib, _ := InfobaseFromConnectingString(connectingString)
+	return ib
+}
+
+func InfobaseFromConnectingString(connectingString string) (Infobase, error) {
+
+	switch {
+	case strings.Contains(connectingString, "Srvr="):
+		return ServerInfobaseFromConnectingString(connectingString)
+	case strings.Contains(connectingString, "File="):
+		return FileInfobaseFromConnectingString(connectingString)
+	default:
+		return nil, ErrorParseConnectingString
+	}
+
+}
+
+func FileInfobaseFromConnectingString(connectingString string) (FileInfoBase, error) {
+
+	if strings.HasPrefix(connectingString, "/IBConnectionString ") {
+		connectingString = strings.TrimPrefix(connectingString, "/IBConnectionString ")
+	}
+
+	var ib FileInfoBase
+	values := strings.Split(connectingString, ";")
+
+	for _, value := range values {
+
+		if len(value) == 0 {
+			continue
+		}
+
+		keyValue := strings.SplitN(value, "=", 2)
+
+		if len(keyValue) != 2 {
+			return FileInfoBase{}, errors.BadConnectString.New("wrong key/value count")
+		}
+
+		key := keyValue[0]
+		val := keyValue[1]
+
+		switch strings.ToLower(key) {
+
+		case "file":
+			ib.File = val
+		case "locale":
+			ib.Locale = val
+		case "usr":
+			ib.Usr = val
+		case "pwd":
+			ib.Pwd = val
+		case "prmod":
+			ib.Prmod = val == "1"
+		case "licdstr":
+			ib.LicDstr = val == "Y"
+		case "zn":
+			var err error
+			ib.Zn, err = parseDatabaseSeparatorList(val)
+			if err != nil {
+				return FileInfoBase{}, err
+			}
+		}
+	}
+
+	if len(ib.File) == 0 {
+		return FileInfoBase{}, errors.BadConnectString.New("wrong file connecting string")
+	}
+
+	return ib, nil
+
+}
+
+func ServerInfobaseFromConnectingString(connectingString string) (ServerInfoBase, error) {
+
+	if strings.HasPrefix(connectingString, "/IBConnectionString ") {
+		connectingString = strings.TrimPrefix(connectingString, "/IBConnectionString ")
+	}
+
+	var ib ServerInfoBase
+	values := strings.Split(connectingString, ";")
+
+	for _, value := range values {
+
+		if len(value) == 0 {
+			continue
+		}
+
+		keyValue := strings.SplitN(value, "=", 2)
+
+		if len(keyValue) != 2 {
+			return ServerInfoBase{}, errors.BadConnectString.New("wrong key/value count")
+		}
+
+		key := keyValue[0]
+		val := keyValue[1]
+
+		switch strings.ToLower(key) {
+
+		case "srvr":
+			ib.Srvr = val
+		case "ref":
+			ib.Ref = val
+		case "usr":
+			ib.Usr = val
+		case "pwd":
+			ib.Pwd = val
+		case "licdstr":
+			ib.LicDstr = val == "Y"
+		case "prmod":
+			ib.Prmod = val == "1"
+		case "zn":
+			var err error
+			ib.Zn, err = parseDatabaseSeparatorList(val)
+			if err != nil {
+				return ServerInfoBase{}, err
+			}
+		}
+	}
+
+	if len(ib.Srvr) == 0 || len(ib.Ref) == 0 {
+		return ServerInfoBase{}, errors.BadConnectString.New("wrong server connecting string")
+	}
+
+	return ib, nil
+}
+
+func parseDatabaseSeparatorList(stringValue string) (DatabaseSeparatorList, error) {
+	// TODO Сделать парсер
+	return DatabaseSeparatorList{}, nil
 }
--- helpers_test.go
+++ helpers_test.go
@@ -4,11 +4,9 @@ import (
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
 	"os"
-	"path"
+	"reflect"
 	"testing"
 )
-
-var pwd, _ = os.Getwd()
 
 type baseTestSuite struct {
 	suite.Suite
@@ -22,64 +20,268 @@ func (s *baseTestSuite) r() *require.Assertions {
 	return s.Require()
 }
 
-type v8TestSuite struct {
-	baseTestSuite
-}
-
-func Test_v8TestSuite(t *testing.T) {
-	suite.Run(t, new(v8TestSuite))
-}
-
-func (t *v8TestSuite) AfterTest(suite, testName string) {
-
-}
-func (t *v8TestSuite) BeforeTest(suite, testName string) {
-
-}
-
-func (t *v8TestSuite) TearDownTest() {
-
-}
-
-func (t *v8TestSuite) TestCreateTempInfobase() {
-
-	ib := NewTempIB()
-
-	err := Run(ib, CreateFileInfobase(ib.File),
-		WithVersion("8.3"),
-		WithTimeout(30),
-	)
-
-	t.r().NoError(err)
-
-	fileBaseCreated, err2 := Exists(path.Join(ib.Path(), "1Cv8.1CD"))
-	t.r().NoError(err2)
-	t.r().True(fileBaseCreated, "Файл базы должен быть создан")
-
-}
-
-func (t *v8TestSuite) TestCreateFileInfobase() {
-
-	tempDir := NewTempDir("", "v8_temp_ib")
-
-	ib := NewFileIB(tempDir)
-
-	err := Run(ib, CreateFileInfobase(ib.File),
-		WithVersion("8.3"),
-		WithTimeout(30),
-	)
-
-	t.r().NoError(err)
-
-	fileBaseCreated, err2 := Exists(path.Join(tempDir, "1Cv8.1CD"))
-	t.r().NoError(err2)
-	t.r().True(fileBaseCreated, "Файл базы должен быть создан")
-
-}
 func Exists(name string) (bool, error) {
 	_, err := os.Stat(name)
 	if os.IsNotExist(err) {
 		return false, err
 	}
 	return true, nil
+}
+
+func TestFileInfobaseFromConnectingString(t *testing.T) {
+	type args struct {
+		connectingString string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    FileInfoBase
+		wantErr bool
+	}{
+		{
+			"simple",
+			args{"File=./file_ib;Locale=ru_RU"},
+			FileInfoBase{
+				File:   "./file_ib",
+				Locale: "ru_RU",
+			},
+			false,
+		},
+		{
+			"error",
+			args{"File ./file_ib2"},
+			FileInfoBase{},
+			true,
+		},
+		{
+			"full",
+			args{"File=./file_ib;Locale=ru_RU;Usr=User;Pwd=Password;Prmod=1;LicDstr=Y"},
+			FileInfoBase{
+				File:   "./file_ib",
+				Locale: "ru_RU",
+				InfoBase: InfoBase{
+					Usr:     "User",
+					Pwd:     "Password",
+					LicDstr: true,
+					Prmod:   true,
+				},
+			},
+			false,
+		},
+
+		{
+			"with IBConnectionString",
+			args{"/IBConnectionString File=./file_ib"},
+			FileInfoBase{
+				File: "./file_ib",
+			},
+			false,
+		},
+		{
+			"with error IBConnectionString",
+			args{"/IBConnectionString=File=./file_ib"},
+			FileInfoBase{},
+			true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := FileInfobaseFromConnectingString(tt.args.connectingString)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("FileInfobaseFromConnectingString() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("FileInfobaseFromConnectingString() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestInfobaseFromConnectingString(t *testing.T) {
+	type args struct {
+		connectingString string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    Infobase
+		wantErr bool
+	}{
+		{
+			"file",
+			args{"File=./file_ib;Locale=ru_RU"},
+			FileInfoBase{
+				File:   "./file_ib",
+				Locale: "ru_RU",
+			},
+			false,
+		},
+		{
+			"server",
+			args{"Srvr=test_server;Ref=test_ib;"},
+			ServerInfoBase{
+				Srvr: "test_server",
+				Ref:  "test_ib",
+			},
+			false,
+		},
+
+		{
+			"no file or server",
+			args{"FFF=./file_ib;Locale=ru_RU"},
+			nil,
+			true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := InfobaseFromConnectingString(tt.args.connectingString)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("InfobaseFromConnectingString() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("InfobaseFromConnectingString() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestInfobaseFromConnectingStringOrNil(t *testing.T) {
+	type args struct {
+		connectingString string
+	}
+	tests := []struct {
+		name string
+		args args
+		want Infobase
+	}{
+		{
+			"file",
+			args{"File=./file_ib;Locale=ru_RU"},
+			FileInfoBase{
+				File:   "./file_ib",
+				Locale: "ru_RU",
+			},
+		},
+		{
+			"server",
+			args{"Srvr=test_server;Ref=test_ib;"},
+			ServerInfoBase{
+				Srvr: "test_server",
+				Ref:  "test_ib",
+			},
+		},
+
+		{
+			"no file or server",
+			args{"FFF=./file_ib;Locale=ru_RU"},
+			nil,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := InfobaseFromConnectingStringOrNil(tt.args.connectingString); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("InfobaseFromConnectingStringOrNil() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestServerInfobaseFromConnectingString(t *testing.T) {
+	type args struct {
+		connectingString string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    ServerInfoBase
+		wantErr bool
+	}{
+		{
+			"simple",
+			args{"Srvr=test_server;Ref=test_ib;"},
+			ServerInfoBase{
+				Srvr: "test_server",
+				Ref:  "test_ib",
+			},
+			false,
+		},
+		{
+			"error",
+			args{"Srvr=test_server"},
+			ServerInfoBase{},
+			true,
+		},
+		{
+			"full",
+			args{"Srvr=test_server;Ref=test_ib;Usr=User;Pwd=Password;Prmod=1;LicDstr=Y"},
+			ServerInfoBase{
+				Srvr: "test_server",
+				Ref:  "test_ib",
+				InfoBase: InfoBase{
+					Usr:     "User",
+					Pwd:     "Password",
+					LicDstr: true,
+					Prmod:   true,
+				},
+			},
+			false,
+		},
+
+		{
+			"with IBConnectionString",
+			args{"/IBConnectionString Srvr=test_server;Ref=test_ib"},
+			ServerInfoBase{
+				Srvr: "test_server",
+				Ref:  "test_ib",
+			},
+			false,
+		},
+		{
+			"with error IBConnectionString",
+			args{"/IBConnectionString=Srvr=test_server;Ref=test_ib"},
+			ServerInfoBase{},
+			true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := ServerInfobaseFromConnectingString(tt.args.connectingString)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ServerInfobaseFromConnectingString() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("ServerInfobaseFromConnectingString() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_parseDatabaseSeparatorList(t *testing.T) {
+	type args struct {
+		stringValue string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    DatabaseSeparatorList
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := parseDatabaseSeparatorList(tt.args.stringValue)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("parseDatabaseSeparatorList() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("parseDatabaseSeparatorList() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
 }
